ce76a99c49914605bd6c4768ff311ff1df540427
Pranav Ballaney <ballaneypranav@gmail.com>
2020-10-25 23:21:33 +0530

    Add video to Model page

--- utils/postbuild.py
+++ utils/postbuild.py
@@ -2,6 +2,16 @@ with open('dist/index.html', 'r') as file:
     contents = file.read()
 
 contents = contents.replace('src="../assets/', 'src="assets/')
+contents = contents.replace('/assets/vid', './assets/vid')
 
 with open('dist/index.html', 'w') as file:
     file.write(contents)
+
+pages_with_videos = ["Model"]
+
+for page in pages_with_videos:
+    with open(f'dist/{page}/index.html', 'r') as file:
+        contents = file.read()
+    contents = contents.replace('//assets/vid/', '../assets/vid/')
+    with open(f'dist/{page}/index.html', 'w') as file:
+        file.write(contents)
